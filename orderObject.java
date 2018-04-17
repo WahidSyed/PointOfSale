f2273893fccc9241b544640e971a4f20d95e734c
Wahid Hussain Syed <wahidsyed.hussain@gmail.com>
2018-04-17 19:11:08 +0530

    modified orderObject.java file to include comments

--- orderObject.java
+++ orderObject.java
@@ -1,18 +1,17 @@
-
-
-
-
-
+// This is Wahid Syed's code
 public class orderObject extends Object
 {
     private double qJames;
     private double pNadine;
 
+// orderObject data initialization
     public orderObject()
     {
         qJames=0;
         pNadine=0;
     }
+
+// orderObject data assignment
     public orderObject(double qJames,double pNadine)
     {
 
